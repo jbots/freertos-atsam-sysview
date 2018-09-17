866d39c1f11689d485a53d424268b0cd7ec79a29
Justyn Butler <commits@justyn.co>
2018-09-17 17:10:18 +0100

    Implement SEGGER_SYSVIEW_X_GetTimestamp for CM0 port.
    Relies on SEGGER_SYSVIEW_TickCnt incremented every tick as
    provided by commit 8cd0165.
    
    This function implementation is provided in SystemView manual,
    Cortex M0/M1 section.

--- FreeRTOSV10.0.0/Source/portable/GCC/ARM_CM0/port.c
+++ FreeRTOSV10.0.0/Source/portable/GCC/ARM_CM0/port.c
@@ -376,3 +376,51 @@ void prvSetupTimerInterrupt( void )
 }
 /*-----------------------------------------------------------*/
 
+
+/*********************************************************************
+*
+* SEGGER_SYSVIEW_X_GetTimestamp()
+*
+* Function description
+* Returns the current timestamp in ticks using the system tick
+* count and the SysTick counter.
+* All parameters of the SysTick have to be known and are set via
+* configuration defines on top of the file.
+*
+* Return value
+* The current timestamp.
+*
+* Additional information
+* SEGGER_SYSVIEW_X_GetTimestamp is always called when interrupts are
+* disabled. Therefore locking here is not required.
+*/
+
+/*********************************************************************
+* SEGGER_SYSVIEW_X_GetTimestamp registers
+***********************************************************************/
+#define SCB_ICSR (*(volatile U32*) (0xE000ED04uL)) // Interrupt Control State Register
+#define SCB_ICSR_PENDSTSET_MASK (1UL << 26) // SysTick pending bit
+#define SYST_RVR (*(volatile U32*) (0xE000E014uL)) // SysTick Reload Value Register
+#define SYST_CVR (*(volatile U32*) (0xE000E018uL)) // SysTick Current Value Register
+
+U32 SEGGER_SYSVIEW_X_GetTimestamp(void) {
+	U32 TickCount;
+	U32 Cycles;
+	U32 CyclesPerTick;
+
+	// Get the cycles of the current system tick.
+	// SysTick is down-counting, subtract the current value from the number of cycles per tick.
+	CyclesPerTick = SYST_RVR + 1;
+	Cycles = (CyclesPerTick - SYST_CVR);
+
+	// Get the system tick count.
+	TickCount = SEGGER_SYSVIEW_TickCnt;
+
+	// If a SysTick interrupt is pending, re-read timer and adjust result
+	if ((SCB_ICSR & SCB_ICSR_PENDSTSET_MASK) != 0) {
+		Cycles = (CyclesPerTick - SYST_CVR);
+		TickCount++;
+	}
+	Cycles += TickCount * CyclesPerTick;
+	return Cycles;
+}
